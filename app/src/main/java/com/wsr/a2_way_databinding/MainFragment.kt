7451352b866bb374310e43d43d06ea8099bf511d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-02-07 00:45:04 +0900

    [add] 元を作成

---
+++ app/src/main/java/com/wsr/a2_way_databinding/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.wsr.a2_way_databinding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
---
+++ app/src/main/java/com/wsr/a2_way_databinding/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.wsr.a2_way_databinding
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application){
+
+}
